95bc00a49b65a058e3e8a41fa59d09797609bd41
vitaminwater <stantin04@gmail.com>
2020-03-02 15:20:48 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>COMING SOON</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
